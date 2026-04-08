de0e12fcabf25dd53412da5c0f7214281644b372
Shakil Ahmad <67405423+shakilahmad630@users.noreply.github.com>
2026-04-08 22:58:21 +0530

    swap

--- swapNumber.java
+++ swapNumber.java
@@ -1,7 +1,6 @@
 package interviewCode;
 public class swapNumber {
     // Swap number without using thord variable
-
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
