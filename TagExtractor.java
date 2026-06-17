131a73afb9b7ca8f211407bae4f1f6b78d087f22
Shakil Ahmad <67405423+shakilahmad630@users.noreply.github.com>
2026-06-17 23:04:57 +0530

    TagExtractor

---
+++ TagExtractor.java
@@ -0,0 +1,31 @@
+package interviewCode;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class TagExtractor {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        while (testCases-- > 0) {
+            String line = in.nextLine();
+
+            Pattern pattern = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher matcher = pattern.matcher(line);
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
+                found = true;
+            }
+
+            if (!found) {
+                System.out.println("None");
+            }
+        }
+
+        in.close();
+    }
+}
