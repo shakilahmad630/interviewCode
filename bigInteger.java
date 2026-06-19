e7e7c294707c184f04f1adebd5aea4e841605013
Shakil Ahmad <67405423+shakilahmad630@users.noreply.github.com>
2026-06-19 23:00:13 +0530

    bigInteger

---
+++ bigInteger.java
@@ -0,0 +1,27 @@
+package interviewCode;
+
+
+import java.io.*;
+        import java.util.*;
+        import java.text.*;
+        import java.math.*;
+        import java.util.regex.*;
+
+public class bigInteger {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BigInteger num1 = sc.nextBigInteger();
+        BigInteger num2 = sc.nextBigInteger();
+        BigInteger num3, num4;
+        if(num1.compareTo(BigInteger.ZERO)>0 && num2.compareTo(BigInteger.ZERO)>0)
+        {
+            num3 = num1.add(num2);
+            num4 = num1.multiply(num2);
+            System.out.println(num3);
+            System.out.print(num4);
+        }
+
+    }
+}
